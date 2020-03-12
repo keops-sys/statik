7f7eca536af66eb7b81848e36b0d6c6dfd5f7ae8
Admin <admin@k3op.com>
2020-03-12 17:42:12 +0100

    't(

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 $ git clone git@github.com:foomo/pagespeed_exporter.git
 $ cd pagespeed_exporter/example
 $ docker-compose up -d
+
+
+docker run -it --rm --name certbot -p 80:80 -p 443:443 -v "/etc/letsencrypt:/etc/letsencrypt" -v "/var/lib/letsencrypt:/var/lib/letsencrypt" certbot/certbot certonly --standalone --email "user@example.com" -d "tls.example.com"
--- start.sh
+++ start.sh
@@ -1,10 +1,12 @@
 #hugo server -D
 #hugo server --bind 127.0.0.1 --port 8080 --disableFastRender
 
-hugo --minify
+#hugo --minify
 
-docker stop landing-statik
-docker rm landing-statik
+#docker stop landing-statik
+#docker rm landing-statik
 
-docker build -t statik .
-docker run --name landing-statik -d -p 8080:80 statik
+#docker build -t statik .
+#docker run --name landing-statik -d -p 8080:80 statik
+
+docker-compose up -d
