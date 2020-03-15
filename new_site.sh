4b087130dbf725163a8896c61907de24b6ad3eb4
Admin <admin@k3op.com>
2020-03-15 23:48:38 +0100

    Update new_site.sh

--- new_site.sh
+++ new_site.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-echo $1
-#mkdir $1
-#cd $1 && git clone git@github.com:keops-sys/statik.git .
-cd $1/ && start.sh $1 $2
+echo "Launch Name:" $1
+echo "Launch url:" $2
+
+name=$1
+hostname=$2
+
+mkdir $1
+cd $1
+
+git clone git@github.com:keops-sys/statik.git .
+
+cd $1
+
+docker stop $1
+docker rm $1
+
+HOSTNAME=$2 NAME=$1 docker-compose up -d
+
